b35010752bee2e45447a5b4b903d59fecb77fed4
Raymondconcept12 <145141947+Raymondconcept12@users.noreply.github.com>
2023-10-12 09:14:28 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    let menubar = document.getElementById("myLink");
+
+    if (menubar.style.display === "block") {
+        menubar.style.display = "none";
+    } else {
+        menubar.style.display = "block";
+    }
+  }
+
+let up = document.getElementById("arrowup")
+
